e5aaec588a4ee49587e70a4a957a378a593c324f
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2021-02-25 20:53:56 +0100

    Add validations to Intercommunality model

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -1,2 +1,7 @@
 class Intercommunality < ApplicationRecord
+  validates :name, :siren, presence: true
+  validates :siren, uniqueness: { case_sensitive: false },
+                    numericality: { only_integer: true },
+                    length: { is: 9 }
+   validates :form, inclusion: { in: %w(ca cu cc met) }
 end
