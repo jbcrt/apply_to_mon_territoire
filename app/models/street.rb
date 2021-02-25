a5f3828f61476a5b286337f582c5e509e3d58a8b
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2021-02-25 20:59:40 +0100

    Add validations to Street model

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,2 +1,14 @@
 class Street < ApplicationRecord
+  validates :title, presence: true
+  validates :from, :to, numericality: { only_integer: true },
+                        allow_nil: true
+  validate :to_cannot_be_inferior_than_from
+
+  private
+
+  def to_cannot_be_inferior_than_from
+    if from && to
+      errors.add(:to, "can't be inferior than 'from' value") if from > to
+    end
+  end
 end
