2aa4e56e2b749e87a2b887ab4500c33ed158b572
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2021-02-25 21:20:14 +0100

    Add .search method to Commune

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -1,8 +1,12 @@
 class Commune < ApplicationRecord
-  belongs_to :intercommunality
+  belongs_to :intercommunality, optional: true
   has_many :commune_streets
   has_many :streets, through: :commune_streets
 
   validates :name, :code_insee, presence: true
   validates :code_insee, length: { is: 5 }
+
+  def self.search(city)
+    Commune.where("lower(name) LIKE ?", "%#{sanitize_sql_like(city.downcase)}%")
+  end
 end
