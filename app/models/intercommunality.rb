7fbe242120d3d11c4053004629908bdd3ccee830
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2021-02-25 21:14:44 +0100

    Create #communes_hash method

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -1,9 +1,13 @@
 class Intercommunality < ApplicationRecord
   has_many :communes
-  
+
   validates :name, :siren, presence: true
   validates :siren, uniqueness: { case_sensitive: false },
                     numericality: { only_integer: true },
                     length: { is: 9 }
-   validates :form, inclusion: { in: %w(ca cu cc met) }
+  validates :form, inclusion: { in: %w(ca cu cc met) }
+
+  def communes_hash
+    self.communes.pluck(:code_insee, :name).to_h
+  end
 end
