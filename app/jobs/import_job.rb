765c9a2ff7a3b6c6c3ca28df433f64d08ecb99df
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2021-02-25 22:07:25 +0100

    Create ImportJob to import the intercommunalities

---
+++ app/jobs/import_job.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+class ImportJob < ApplicationJob
+  queue_as :default
+
+  def perform(csv)
+    csv_options = { col_sep: ';', quote_char: nil, headers: :first_row, encoding: "ISO8859-1" }
+    intercommunality_form = {
+      "CA" => "ca",
+      "CU" => "cu",
+      "CC" => "cc",
+      "METRO" => "met"
+    }
+
+    CSV.foreach(csv, csv_options) do |row|
+      intercommunality = Intercommunality.where(name: row['nom_complet'], siren: row['siren_epci'], form: intercommunality_form[row['form_epci']]).first_or_create
+    end
+  end
+end
